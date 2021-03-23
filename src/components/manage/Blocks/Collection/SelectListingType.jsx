3cacf4df19f7fc07d4fe110689db828c7916283b
nileshgulia1 <nileshgulia@gmail.com>
2021-03-23 22:01:38 +0530

    icon fix

--- src/components/manage/Blocks/Collection/SelectListingType.jsx
+++ src/components/manage/Blocks/Collection/SelectListingType.jsx
@@ -2,6 +2,7 @@ import appsSVG from '@plone/volto/icons/apps.svg';
 import contentListingSVG from '@plone/volto/icons/content-listing.svg';
 import tableSVG from '@plone/volto/icons/table.svg';
 import { Button, Form, Grid } from 'semantic-ui-react';
+import Icon from '@plone/volto/components';
 
 import React, { Component } from 'react';
 
